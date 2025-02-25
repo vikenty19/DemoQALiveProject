a000f46647cf6ee90092a4f510af5e0c6de06a3a
vikenty19 <vikenty.plakhov@testpro.io>
2025-02-25 15:55:19 -0500

    Hooks.java is added

--- src/test/java/com/tutorialsninja/automation/stepdef/Register.java
+++ src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -1,4 +1,6 @@
 package com.tutorialsninja.automation.stepdef;
+import com.tutorialsninja.automation.base.Base;
+import com.tutorialsninja.automation.config.PropertyFileReader;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.*;
 public class Register {
@@ -6,6 +8,8 @@ public class Register {
 
             @Given("I launch the app")
             public void i_launch_the_app(){
+
+                Base.driver.get(Base.reader.getUrl());
             }
             @And("^I navigate to Account Registration page$")
             public void i_navigate_to_Account_Registration_Page()    {
